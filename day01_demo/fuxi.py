d1ed8ce50f23e81a313f4692b44ad44f51cba82b
gyl <aasssitya@163.com>
2020-04-24 13:22:24 +0800

    add

--- day01_demo/fuxi.py
+++ day01_demo/fuxi.py
@@ -20,17 +20,31 @@ print(dict3)
 dict4 = {key: value for key, value in dict3.items() if key > 5}
 print(dict4)
 
-def addSum(v1,v2):
-    return v1+v2
 
-#rpd846
+def addSum(v1, v2):
+    return v1 + v2
 
 def showMessage():
     """显示信息"""
-    value=addSum(12,23)
+    value = addSum(12, 23)
     print(value)
     return "hello world"
+
+
 print(help(showMessage))  # 查看函数说明文档
 print(showMessage())
 
 
+class Student():
+    def __init__(self):
+        self.name, self.age, self.tz = '', 0, 0.00
+
+    def showInfo(self):
+        return self.name + ":" + str(self.age) + ":" + str(self.tz)
+
+
+stu = Student()
+stu.name = 'tom'
+stu.age = 130
+stu.tz = 12.3
+print(stu.showInfo())
